ef1682a6dd09687df31eab71e1e5dfb583e4d327
farooqalwi <farooqalwi313@gmail.com>
2023-01-27 15:05:26 +0500

    icon and meta name changed

--- pages/index.js
+++ pages/index.js
@@ -683,7 +683,7 @@ const Home = () => {
     <>
       <Head>
         <title>OR Simulator</title>
-        <meta name="description" content="A simulator based on queueing model M/M/2" />
+        <meta name="OR Simulator" content="A simulator based on queueing model M/M/2" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossOrigin="anonymous"></link>
         {/* <link rel="stylesheet" href="../styles/bootstrap-5.3.0-alpha1-dist/css/bootstrap.min.css" crossOrigin="anonymous" /> */}
